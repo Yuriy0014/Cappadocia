07471a9aa6760535634fbb259285b51a622f810e
Yuriy_log <ylogachev-sfedu@mail.ru>
2020-10-21 23:11:26 +0300

    Finished Fairy tail section

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ $(function(){
     $('.fairy-tail__slider').slick({
         prevArrow: '<button type="button" class="slick-btn slick-prev">​<img src="images/arrow-prev.svg" alt=""></button>',
         nextArrow: '<button type="button" class="slick-btn slick-next">​<img src="images/arrow-next.svg" alt=""></button>',
-        autoplay: true
+        autoplay: true,
+        fade: true
     });
 });
